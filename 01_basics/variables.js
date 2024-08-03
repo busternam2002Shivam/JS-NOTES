83ee9e686366ea275fe1472bc9721f89df68478a
busternam2002Shivam <94841234+busternam2002Shivam@users.noreply.github.com>
2024-08-03 10:07:17 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const id = 110062
+let Name = "rohan"
+var lunch = "paratha"
+aroundcity = "Delhi"
+let accountstate
+
+// id = 2node //node allowed
+
+console.log(id);
+
+Name = "Shivam"
+lunch = "chapati"
+aroundcity = "SouthDelhi"
+// prefer not to use var becoz of issue in block scope and functional scope
+console.table([id,Name,lunch,aroundcity,accountstate]);
+
+
+
